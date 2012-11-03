7f72771c0a7a2ee618a73139561c68a3fc7a17c8
Michael Penkov <misha.penkov@gmail.com>
2012-11-03 18:22:44 +0900

    Added plot_au.py

---
+++ scripts/plot_au.py
@@ -0,0 +1,24 @@
+"""Plot the waveform of the signal in the specified AU file."""
+
+import matplotlib.pyplot as plt
+import sys
+from struct import unpack, calcsize
+
+def read_au(fname):
+    """Read the AU file.  Return the sampling rate and samples in a list."""
+    fin = open(fname, "rb")
+    payload = fin.read()
+
+    assert payload[:4] == ".snd"
+    header_len = calcsize(">5L")
+    _, nsamples, encoding, sample_rate, _ = unpack(">5L", payload[4:4+header_len])
+    samples = unpack("%db" % nsamples, payload[4+header_len:])
+    return sample_rate, samples
+
+if __name__ == "__main__":
+    sample_rate, samples = read_au(sys.argv[1])
+    plt.title("%s (%dHz)" % (sys.argv[1], sample_rate))
+    plt.plot(range(len(samples)), samples)
+    plt.xlabel("sample number")
+    plt.ylabel("value")
+    plt.show()
